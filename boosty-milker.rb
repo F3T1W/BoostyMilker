cee85034ec41cd393b0c75fdaed3a2691bcea8d1
Vlad Matiushin <particular0010abyss@gmail.com>
2026-01-23 13:12:37 +0300

    Sync sha256 with homebrew-tap

--- boosty-milker.rb
+++ boosty-milker.rb
@@ -4,7 +4,7 @@ class BoostyMilker < Formula
   desc "CLI tool to download photos from Boosty"
   homepage "https://github.com/F3T1W/BoostyMilker"
   url "https://github.com/F3T1W/BoostyMilker/archive/refs/tags/v1.0.3.tar.gz"
-  sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
+  sha256 "ee3fed85b56fecd1824ef9d26523ea5c2eb5d91648d31ae5c31835e3d355cfc3"
   license "MIT"
 
   depends_on "python@3.11"
